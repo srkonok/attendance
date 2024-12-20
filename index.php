d723af6fdc506509e3d09e5e09d3567d997d1d7c
Shahriar R. Konok <srkonok20@gmail.com>
2024-12-20 12:07:50 +0600

    favicon ico

--- index.php
+++ index.php
@@ -117,6 +117,7 @@ $attendees = $stmt->fetchAll();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cloud Computing Attendance</title>
+    <link rel="icon" href="images/favicon_io/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="style.css">
     <script src="attendance.js" defer></script>
 </head>
