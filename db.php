acb6f946f5ebe13f2981cfc0002d9645143cca49
Shahriar R. Konok <srkonok20@gmail.com>
2025-02-17 18:44:21 +0600

    update

--- db.php
+++ db.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+
 $host = $_ENV['DB_HOST'];
 $dbname = $_ENV['DB_NAME'];
 $username = $_ENV['DB_USERNAME'];
